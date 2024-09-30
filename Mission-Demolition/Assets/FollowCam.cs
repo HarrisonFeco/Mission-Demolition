9a827763bea403a74a1dc8e284599df072862b75
LuisH004 <luhaby7@gmail.com>
2024-09-30 13:47:33 -0500

    on pg 718 making walls and slabs

--- Mission-Demolition/Assets/FollowCam.cs
+++ Mission-Demolition/Assets/FollowCam.cs
@@ -20,8 +20,22 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate ()
     {
-        if(POI == null) return;
-        Vector3 destination = POI.transform.position;
+        // if(POI == null) return;
+        // Vector3 destination = POI.transform.position;
+
+        Vector3 destination = Vector3.zero;
+        if(POI != null)
+        {
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if((poiRigid != null) && poiRigid.IsSleeping())
+            {
+                POI = null;
+            }
+        }
+        if(POI != null)
+        {
+            destination = POI.transform.position;
+        }
 
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
@@ -29,6 +43,7 @@ public class FollowCam : MonoBehaviour
         destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
 
