3ae77261500c6a983b86c26407b0d2e4ab797d29
HarrisonFeco <84511537+HarrisonFeco@users.noreply.github.com>
2024-10-03 13:53:21 -0500

    Page 737 Adding UI

--- Mission-Demolition/Assets/Scripts/ProjectileLine.cs
+++ Mission-Demolition/Assets/Scripts/ProjectileLine.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class ProjectileLine : MonoBehaviour
 {
+    static List <ProjectileLine> PROJ_LINES = new List<ProjectileLine>();
+    private const float DIM_MULT = 0.75F;
+
     private LineRenderer _line;
     private bool _drawing = true;
     private Projectile _projectile;
@@ -16,6 +19,7 @@ public class ProjectileLine : MonoBehaviour
         _line.positionCount = 1;
         _line.SetPosition(0, transform.position);
         _projectile = GetComponentInParent<Projectile>();
+        ADD_LINE(this);
     }
     void FixedUpdate()
     {
@@ -34,5 +38,19 @@ public class ProjectileLine : MonoBehaviour
             }
         }
     }
-
+    private void OnDestroy()
+    {
+        PROJ_LINES.Remove(this);
+    }
+    static void ADD_LINE(ProjectileLine newLine)
+    {
+        Color col;
+        foreach(ProjectileLine pl in PROJ_LINES)
+        {
+            col = pl._line.startColor;
+            col = col * DIM_MULT;
+            pl._line.startColor = pl._line.endColor = col;
+        }
+        PROJ_LINES.Add(newLine);
+    }
 }
--- Mission-Demolition/Assets/Scripts/Slingshot.cs
+++ Mission-Demolition/Assets/Scripts/Slingshot.cs
@@ -7,6 +7,7 @@ public class Slingshot : MonoBehaviour
     [Header("Inscribed")]
     public GameObject projectilePrefab;
     public float velocityMult = 10f;
+    public GameObject projLinePrefab;
 
     public GameObject launchPoint;
     public Vector3 launchPos;
@@ -67,6 +68,7 @@ public class Slingshot : MonoBehaviour
             projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
             projRB.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
+            Instantiate<GameObject>(projLinePrefab, projectile.transform);
             projectile = null;
         }
     }
